e8a3f4588f065c7cdc4d6a1e998724228aa81a2f
Luka G <gostencnikluka@gmail.com>
2022-08-20 21:45:27 +0200

    Add files via upload

---
+++ conf.d/aliases.fish
@@ -0,0 +1,11 @@
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+alias py python
+alias add "git add"
+alias checkout "git checkout"
+alias clone "git clone"
+alias commit "git commit -S"
+alias pull "git pull"
+alias push "git push"
+alias stat "git status"
---
+++ conf.d/exports.fish
@@ -0,0 +1,4 @@
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx PF_INFO "ascii title os host wm pkgs shell editor"
+
