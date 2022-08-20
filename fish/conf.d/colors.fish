934b87022b8f6c8f35b58527ec5fc548e4c3968f
Luka G <gostencnikluka@gmail.com>
2022-08-20 21:46:26 +0200

    Add files via upload

---
+++ fish/conf.d/colors.fish
@@ -0,0 +1,11 @@
+set -U fish_color_normal white
+set -U fish_color_command green
+set -U fish_color_keyword purple 
+set -U fish_color_quote yellow
+set -U fish_color_error red --bold
+set -U fish_color_autosuggestion brblack
+set -U fish_color_param white
+set -U fish_color_comment brblack --italics
+set -U fish_color_valid_path --underline
+set -U fish_color_cancel --reverse --bold red
+set -U fish_pager_color_progress cyan --bold
